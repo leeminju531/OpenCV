d456b89f293900ede0864f798381b90c48bec876
leeminju <dlalswn531@naver.com>
2021-05-13 16:51:28 +0900

    all of scan method

---
+++ 1.ImageScanning(LUT)/ScanMethod/README.md
@@ -0,0 +1,6 @@
+이미지 스캔하는 방법에 대해
+1. Classic C Style 접근
+2. Iterator 접근
+3. Random Access 접근
+
+을 차례로 구현된 함수가 포함되어 있습니다.
---
+++ 1.ImageScanning(LUT)/ScanMethod/main.cpp
@@ -0,0 +1,147 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/utility.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+Mat& ScanImageAndReduceC(Mat& I, const uchar* table);
+Mat& ScanImageAndReduceIterator(Mat& I, const uchar* table);
+Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* table);
+
+
+int main()
+{
+    string img_path = "starry_night.jpg";
+    Mat I = imread(img_path, IMREAD_COLOR);
+    //Mat I = imread(img_path, IMREAD_GRAYSCALE);
+    if (I.empty())
+    {
+        cout << "The image" << img_path << " could not be loaded." << endl;
+        return -1;
+    }
+
+    int divideWith = 150;
+    uchar table[256];
+    for (int i = 0; i < 256; ++i)
+        table[i] = (uchar)(divideWith * (i / divideWith));
+
+    Mat J;
+   
+    for (int i = 0; i < 100; ++i)
+    {
+        cv::Mat clone_i = I.clone();
+        J = ScanImageAndReduceC(clone_i, table);
+    }
+    cv::Mat clone_i = I.clone();
+    J = ScanImageAndReduceC(clone_i, table);
+    //J = ScanImageAndReduceIterator(clone_i, table);
+    //J = ScanImageAndReduceRandomAccess(clone_i, table);
+
+
+    imshow("Original Image ", I);
+    imshow("after LookupTable(color reduced Image)", J);
+    waitKey(0);
+
+    return 0;
+}
+
+////! [scan-c]
+Mat& ScanImageAndReduceC(Mat& I, const uchar* table)
+{
+    // cv_assert(expr)
+    // cv_assert()는 opencv에서 제공하는 매크로 함수
+    // 괄호 안의 조건식이 참(true)이면 정상적으로 함수를 진행
+    // 거짓(false)이면 에러를 발생(커맨드창에 에러 메시지를 출력)
+    CV_Assert(I.depth() == CV_8U);
+    // 1byte unsighned char 형식을 허용하겠다는 의미
+
+    int channels = I.channels();
+    int nRows = I.rows;
+    int nCols = I.cols * channels;
+    if (I.isContinuous())
+    {
+        nCols *= nRows;
+        nRows = 1;
+    }
+    int i, j;
+    uchar* p;
+    for (i = 0; i < nRows; ++i)
+    {
+        p = I.ptr<uchar>(i);
+        for (j = 0; j < nCols; ++j)
+        {
+            p[j] = table[p[j]];
+        }
+    }
+    return I;
+}
+
+//! [scan-iterator]
+Mat& ScanImageAndReduceIterator(Mat& I, const uchar* const table)
+{
+    // accept only char type matrices
+    CV_Assert(I.depth() == CV_8U);
+
+    const int channels = I.channels();
+    switch(channels)
+    {
+    case 1:
+        {
+            MatIterator_<uchar> it, end;
+            for( it = I.begin<uchar>(), end = I.end<uchar>(); it != end; ++it)
+                *it = table[*it];
+            break;
+        }
+    case 3:
+        {
+            MatIterator_<Vec3b> it, end;
+            for( it = I.begin<Vec3b>(), end = I.end<Vec3b>(); it != end; ++it)
+            {
+                (*it)[0] = table[(*it)[0]];
+                (*it)[1] = table[(*it)[1]];
+                (*it)[2] = table[(*it)[2]];
+            }
+        }
+    }
+
+    return I;
+}
+//! [scan-iterator]
+
+//! [scan-random]
+Mat& ScanImageAndReduceRandomAccess(Mat& I, const uchar* const table)
+{
+    // accept only char type matrices
+    CV_Assert(I.depth() == CV_8U);
+
+    const int channels = I.channels();
+    switch(channels)
+    {
+    case 1:
+        {
+            for( int i = 0; i < I.rows; ++i)
+                for( int j = 0; j < I.cols; ++j )
+                    I.at<uchar>(i,j) = table[I.at<uchar>(i,j)];
+            break;
+        }
+    case 3:
+        {
+         Mat_<Vec3b> _I = I;
+
+         for( int i = 0; i < I.rows; ++i)
+            for( int j = 0; j < I.cols; ++j )
+               {
+                   _I(i,j)[0] = table[_I(i,j)[0]];
+                   _I(i,j)[1] = table[_I(i,j)[1]];
+                   _I(i,j)[2] = table[_I(i,j)[2]];
+            }
+         I = _I;
+         break;
+        }
+    }
+
+    return I;
+}
+//! [scan-random]
