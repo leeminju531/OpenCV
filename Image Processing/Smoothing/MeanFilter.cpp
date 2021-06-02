63c00613b9b04afcbd9e063febea319fc17f1d80
leeminju <dlalswn531@naver.com>
2021-06-02 13:26:20 +0900

    mean filter

---
+++ Image Processing/Smoothing/MeanFilter.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv)
+{
+    const char* filename = "lena.jpg";
+
+    Mat src = imread(filename, IMREAD_COLOR);
+    if (src.empty())
+    {
+        cout << filename << "Load Fail"<<endl;
+        return EXIT_FAILURE;
+    }
+
+    Mat dst = src.clone();
+ 
+    //![blur]
+    blur(src, dst, Size(20,20), Point(-1, -1));
+
+    imshow("Origin", src);
+    imshow("mean Filter", dst);
+    waitKey(0);
+    
+    return 0;
+}
+
